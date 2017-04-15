6ad8da183437eb6ded54ee0368ab38a4836678d0
kumardesappan <kumar.adk@gmail.com>
2017-04-15 05:21:07 +0000

    [C-CPP C9] Tested HW with function and multiple files in C9

---
+++ c-cpp/hello-world/addVars.c
@@ -0,0 +1,4 @@
+int addVars(int a, int b)
+{
+  return (a+b);
+}
--- c-cpp/hello-world/helloWorld.c
+++ c-cpp/hello-world/helloWorld.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int addVars(int a, int b);
+
 int main()
 {
-  int a,b;
+  int a,b,c;
   a = 101;
   b = 102;
   b += a;
   
   a = b+25;
+  
+  c = addVars(a,b);
 
   printf("Hello World\n");
-  printf("A : %d, B : %d\n", a, b);
+  printf("A : %d, B : %d, C : %d \n", a, b, c);
 }
